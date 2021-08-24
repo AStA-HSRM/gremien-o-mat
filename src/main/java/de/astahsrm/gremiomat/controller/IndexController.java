2683835e9edba1e1df7b3465d5ab30975bd6c0e5
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-08-24 10:13:46 +0200

    Enable debug mode, set up run configuration

--- src/main/java/de/astahsrm/gremiomat/controller/IndexController.java
+++ src/main/java/de/astahsrm/gremiomat/controller/IndexController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping
 public class IndexController {
     
-    @GetMapping
+    @GetMapping("/test")
     public String getHome(Model m) {
         return "home";
     }
