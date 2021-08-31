89a3508532dfd8326d3cf69408812f1061e0fa07
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-08-31 22:57:18 +0200

    Remove redundant comment

--- src/main/java/de/astahsrm/gremiomat/GremiomatApplication.java
+++ src/main/java/de/astahsrm/gremiomat/GremiomatApplication.java
@@ -3,7 +3,6 @@ package de.astahsrm.gremiomat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
 @SpringBootApplication
 public class GremiomatApplication {
 
