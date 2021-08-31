1bfe871b828932f018cba81f146bb24a23452812
Nico Ismaili <nico@ismaili.de>
2021-08-31 21:55:21 +0200

    fix: GremiumController prototype

--- src/main/java/de/astahsrm/gremiomat/gremium/GremiumController.java
+++ src/main/java/de/astahsrm/gremiomat/gremium/GremiumController.java
@@ -1,15 +1,35 @@
 package de.astahsrm.gremiomat.gremium;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @Controller
 public class GremiumController {
-    @GetMapping(value="{gremium}")
-    public String getMethodName(@PathVariable("gremium") String gremium) {
-        // TODO something something get Gremium by id...
-        return "gremium_info";
-    };
+
+    @Autowired
+    private GremiumService gremiumService;
+
+    @GetMapping("gremien")
+    public String getGremien(Model m) {
+        m.addAttribute("all_gremiens", gremiumService.getAllGremiumsSortedByName());
+        return "gremiens";
+    }
+
+    @GetMapping("gremien/{abbrv}")
+    public String getMethodName(@PathVariable long id, Model m) {
+        Optional<Gremium> gremium = gremiumService.getGremiumById(id);
+        if(gremium.isPresent()) {
+            m.addAttribute("gremium", gremium);
+            return "gremium_info";
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
+    }
 }
