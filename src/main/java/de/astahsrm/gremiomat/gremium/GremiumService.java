4ccf0df0fb0d1aa7e6aa0ece33e5b53d7e3a8b1f
Nico Ismaili <nico@ismaili.de>
2021-08-31 21:32:09 +0200

    add: getAllGremien() to Gremienservice and its implementation

--- src/main/java/de/astahsrm/gremiomat/gremium/GremiumService.java
+++ src/main/java/de/astahsrm/gremiomat/gremium/GremiumService.java
@@ -1,9 +1,14 @@
 package de.astahsrm.gremiomat.gremium;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GremiumService {
     public Optional<Gremium> getGremiumById(Long id);
+
+    public List<Gremium> getAllGremiumsSortedByName();
+
     public void delGremium(Long id);
+
     public Gremium saveGremium(Gremium gremium);
 }
--- src/main/java/de/astahsrm/gremiomat/gremium/GremiumServiceImpl.java
+++ src/main/java/de/astahsrm/gremiomat/gremium/GremiumServiceImpl.java
@@ -1,10 +1,13 @@
 package de.astahsrm.gremiomat.gremium;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
-public class GremiumServiceImpl implements GremiumService{
+public class GremiumServiceImpl implements GremiumService {
 
     @Autowired
     private GremiumRepository gremiumRepository;
@@ -16,7 +19,7 @@ public class GremiumServiceImpl implements GremiumService{
 
     @Override
     public void delGremium(Long id) {
-        gremiumRepository.deleteById(id); 
+        gremiumRepository.deleteById(id);
     }
 
     @Override
@@ -24,5 +27,9 @@ public class GremiumServiceImpl implements GremiumService{
         return gremiumRepository.save(gremium);
     }
 
-    
+    @Override
+    public List<Gremium> getAllGremiumsSortedByName() {
+        return gremiumRepository.findAll(Sort.by(Direction.DESC, "name"));
+    }
+
 }
