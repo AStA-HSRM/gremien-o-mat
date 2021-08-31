6e6cc4c5a587e28f0b4432cf4c920bdf1fd84832
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-08-31 22:57:33 +0200

    Add request mapping to make code look nicer

--- src/main/java/de/astahsrm/gremiomat/gremium/GremiumController.java
+++ src/main/java/de/astahsrm/gremiomat/gremium/GremiumController.java
@@ -8,22 +8,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 
 @Controller
+@RequestMapping("/gremien")
 public class GremiumController {
 
     @Autowired
     private GremiumService gremiumService;
 
-    @GetMapping("gremien")
+    @GetMapping("")
     public String getGremien(Model m) {
         m.addAttribute("all_gremiens", gremiumService.getAllGremiumsSortedByName());
         return "gremiens";
     }
 
-    @GetMapping("gremien/{abbrv}")
+    @GetMapping("/{abbrv}")
     public String getGremiumInfo(@PathVariable long id, Model m) {
         Optional<Gremium> gremium = gremiumService.getGremiumById(id);
         if(gremium.isPresent()) {
@@ -33,7 +35,7 @@ public class GremiumController {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
     }
 
-    @GetMapping("gremien/add")
+    @GetMapping("/add")
     public String addGremium(Model m) {
         Gremium gremium = new Gremium();
         gremium.setAbbreviation("tst");
