6f2eeedf18bba66e7d228f9f51bf838b3f9d5ab6
Nico Ismaili <nico@ismaili.de>
2021-09-08 20:41:51 +0200

    fix: gremiumid to gremium

--- src/main/java/de/astahsrm/gremiomat/gremium/Query.java
+++ src/main/java/de/astahsrm/gremiomat/gremium/Query.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Query {
-    
     @Id
     @GeneratedValue
     private long id;
@@ -22,10 +21,38 @@ public class Query {
 
     @NotBlank
     @OneToMany
-    private long gremiumId;
+    private Gremium gremium;
 
     public Query() {
         this.text = "";
+        this.gremium = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Query [gremium=" + gremium + ", id=" + id + ", text=" + text + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Query other = (Query) obj;
+        if (id != other.id)
+            return false;
+        return true;
     }
 
     public long getId() {
@@ -50,5 +77,13 @@ public class Query {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public Gremium getGremium() {
+        return gremium;
+    }
+
+    public void setGremium(Gremium gremium) {
+        this.gremium = gremium;
     }
 }
