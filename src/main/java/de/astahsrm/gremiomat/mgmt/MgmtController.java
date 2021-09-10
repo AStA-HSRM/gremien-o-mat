303196086fa53dc255ff3d4cac57c9bc3839cf45
Nico Ismaili <nico@ismaili.de>
2021-09-10 11:26:49 +0200

    add: mgmtcontroller to redirect logged in users

---
+++ src/main/java/de/astahsrm/gremiomat/mgmt/MgmtController.java
@@ -0,0 +1,49 @@
+package de.astahsrm.gremiomat.mgmt;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+import de.astahsrm.gremiomat.security.MgmtUserServiceImpl;
+import de.astahsrm.gremiomat.security.SecurityConfig;
+
+@Controller
+@RequestMapping("/mgmt")
+public class MgmtController {
+
+    @Autowired
+    MgmtUserServiceImpl mgmtUserServiceImpl;
+
+    @GetMapping
+    public String getGremien(Model m, Principal user) {
+        // For testing purposes:
+        if (user.getName().equalsIgnoreCase(SecurityConfig.ADMIN)) {
+            return "redirect:/admin";
+        }
+        if (user.getName().equalsIgnoreCase(SecurityConfig.USER)) {
+            return "redirect:/user";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Cannot find user: " + user.getName() + " in database!");
+        }
+        /* Implementation with DB:
+        String userRole = mgmtUserServiceImpl.getRoleOfUserById(user.getName());
+        if (userRole.equals(SecurityConfig.ADMIN)) {
+            return "redirect:/admin";
+        }
+        if (userRole.equals(SecurityConfig.USER)) {
+            return "redirect:/user";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Cannot find user: " + user.getName() + " in database!");
+        }
+        */
+    }
+
+}
