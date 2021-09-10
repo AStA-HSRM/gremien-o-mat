26847262a34a8dea0150dbc236312a72ce601c06
Nico Ismaili <nico@ismaili.de>
2021-09-10 11:27:57 +0200

    fix: reformatted and implemented auth through db in SecurityConfig.java

--- src/main/java/de/astahsrm/gremiomat/security/SecurityConfig.java
+++ src/main/java/de/astahsrm/gremiomat/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package de.astahsrm.gremiomat.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,36 +19,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public static final String ADMIN = "ADMIN";
     public static final String USER = "USER";
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    /* 
+    @Autowired
+    private MgmtUserDetailsService mgmtUserDetailsService;
+    */
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/assets/**", "/css/**", "/favicon/**", "/img/**").permitAll()
+                .antMatchers("/gremien/**", "/gremien**").permitAll()
+                .antMatchers("/mgmt", "/logout").permitAll()
+                .antMatchers(HttpMethod.DELETE).hasRole(ADMIN)
+                .antMatchers("/user*", "/user/*").authenticated()
+                .anyRequest().hasAnyRole(ADMIN)
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/mgmt")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder authmanagerbuilder) throws Exception {
         PasswordEncoder pwenc = passwordEncoder();
-        authmanagerbuilder.inMemoryAuthentication().withUser("user").password(pwenc.encode("user")).roles("USER").and()
-                .withUser("admin").password(pwenc.encode("admin")).roles(USER, ADMIN);
+        authmanagerbuilder.inMemoryAuthentication()
+            .withUser("user").password(pwenc.encode("user")).roles(USER)
+        .and()
+            .withUser("admin").password(pwenc.encode("admin")).roles(ADMIN);
+        // User-auth with database:
+        // authmanagerbuilder.userDetailsService(mgmtUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/assets/**", "/css/**", "/favicon/**", "/img/**").permitAll()
-            .antMatchers("/gremien/**", "/gremien**").permitAll()
-            .antMatchers("/register", "/logout").permitAll()
-            .antMatchers(HttpMethod.DELETE).hasRole(ADMIN)
-            .antMatchers("/user*", "/user/*").authenticated()
-            .anyRequest().hasAnyRole(ADMIN)
-                .and()
-                    .formLogin()
-                    //.loginPage("/login")
-                    .defaultSuccessUrl("/userseite")
-                    .permitAll()
-                .and()
-                    .logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/")
-                    .permitAll();
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
