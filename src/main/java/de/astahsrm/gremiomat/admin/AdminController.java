320a86bd5660b34723e985e847991d41a4050f88
Nico Ismaili <nico@ismaili.de>
2021-09-10 13:01:43 +0200

    add: csv upload mappings

--- src/main/java/de/astahsrm/gremiomat/admin/AdminController.java
+++ src/main/java/de/astahsrm/gremiomat/admin/AdminController.java
@@ -2,6 +2,7 @@ package de.astahsrm.gremiomat.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,16 @@ public class AdminController {
     public String getAdminPage() {
         return "mgmt/admin/admin-overview";
     }
+
+    @GetMapping("/csv-user-upload")
+    public String getCSVPage() {
+        return "mgmt/admin/csv-user-upload";
+    }
+    
+    @PostMapping("/csv-user-upload")
+    public String processUserCSV() {
+        return "";
+    }
+
 
 }
