a12481dd4563053869429d91c164ddfe8f9f1c94
Nico Ismaili <nico@ismaili.de>
2021-09-15 13:21:28 +0200

    fix: deleting a candidate now deletes him in all gremien too

--- src/main/java/de/astahsrm/gremiomat/candidate/CandidateServiceImpl.java
+++ src/main/java/de/astahsrm/gremiomat/candidate/CandidateServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import de.astahsrm.gremiomat.gremium.Gremium;
+import de.astahsrm.gremiomat.gremium.GremiumService;
 import javassist.NotFoundException;
 
 @Service
@@ -15,6 +17,9 @@ public class CandidateServiceImpl implements CandidateService {
 
     @Autowired
     private CandidateRepository candidateRepository;
+
+    @Autowired
+    private GremiumService gremiumService;
 
     @Override
     public Candidate saveCandidate(Candidate candidate) {
@@ -28,6 +33,18 @@ public class CandidateServiceImpl implements CandidateService {
 
     @Override
     public void delCandidate(String candidateEmail) {
+        Optional<Candidate> cOptional = getCandidateById(candidateEmail);
+        if (cOptional.isPresent()) {
+            Candidate c = cOptional.get();
+            for (Gremium g : c.getGremien()) {
+                Optional<Gremium> gOptional = gremiumService.getGremiumByAbbr(g.getAbbr());
+                if (gOptional.isPresent()) {
+                    Gremium gremium = gOptional.get();
+                    gremium.delCandidate(c);
+                    gremiumService.saveGremium(gremium);
+                }
+            }
+        }
         candidateRepository.deleteById(candidateEmail);
     }
 
