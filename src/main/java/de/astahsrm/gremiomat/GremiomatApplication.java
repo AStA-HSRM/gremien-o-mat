6134c78392b55db16744e53ba0bcae54d7f60e6f
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-09-16 18:40:57 +0200

    fix: remove WAR build target and config

--- src/main/java/de/astahsrm/gremiomat/GremiomatApplication.java
+++ src/main/java/de/astahsrm/gremiomat/GremiomatApplication.java
@@ -2,16 +2,9 @@ package de.astahsrm.gremiomat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GremiomatApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GremiomatApplication.class);
-	}
+public class GremiomatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GremiomatApplication.class, args);
