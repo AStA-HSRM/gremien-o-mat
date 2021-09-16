3d6b8d6f0429267b1ecd6ee6bfb6efe1b3c67c90
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-09-16 18:16:55 +0200

    add: SpringBootServletInitializer for WAR deployment

--- src/main/java/de/astahsrm/gremiomat/GremiomatApplication.java
+++ src/main/java/de/astahsrm/gremiomat/GremiomatApplication.java
@@ -2,9 +2,16 @@ package de.astahsrm.gremiomat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GremiomatApplication {
+public class GremiomatApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GremiomatApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GremiomatApplication.class, args);
