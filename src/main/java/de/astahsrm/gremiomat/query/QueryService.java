9e682ec7669d9bf84d3fd72bb20677596042a362
Nico Ismaili <nico@ismaili.de>
2021-09-19 14:53:26 +0200

    add: string constant for query errors

--- src/main/java/de/astahsrm/gremiomat/query/QueryService.java
+++ src/main/java/de/astahsrm/gremiomat/query/QueryService.java
@@ -3,6 +3,7 @@ package de.astahsrm.gremiomat.query;
 import java.util.Optional;
 
 public interface QueryService {
+    public static final String QUERY_NOT_FOUND = "No such Query exists!";
     public Query saveQuery(Query query);
     public Optional<Query> getQueryByQueryTxt(String queryTxt);
     public void delQueryByQueryTxt(String queryTxt);
