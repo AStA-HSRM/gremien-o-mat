9eeb940f0b3dcbf116def58fe0e90c5aca1afa82
Nico Ismaili <nico@ismaili.de>
2021-09-19 14:53:10 +0200

    fix: imports

--- src/test/java/de/astahsrm/gremiomat/GremiomatApplicationTests.java
+++ src/test/java/de/astahsrm/gremiomat/GremiomatApplicationTests.java
@@ -3,10 +3,8 @@ package de.astahsrm.gremiomat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import de.astahsrm.gremiomat.password.PasswordService;
 import de.astahsrm.gremiomat.password.PasswordServiceImpl;
 
 @SpringBootTest
