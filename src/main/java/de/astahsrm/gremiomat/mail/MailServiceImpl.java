6150abea352743e79e2a100c0bf7d581c0840a87
Nico Ismaili <ismailinico@gmail.com>
2021-09-20 20:36:04 +0200

    fix: imports

--- src/main/java/de/astahsrm/gremiomat/mail/MailServiceImpl.java
+++ src/main/java/de/astahsrm/gremiomat/mail/MailServiceImpl.java
@@ -1,7 +1,5 @@
 package de.astahsrm.gremiomat.mail;
 
-import javax.websocket.server.ServerEndpoint;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
