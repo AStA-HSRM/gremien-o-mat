1d503dacc64a3118812d7bc45ef96ac953c3d549
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-09-20 10:34:47 +0200

    fix: redundant user password encoding during retrieval

--- src/main/java/de/astahsrm/gremiomat/security/MgmtUserDetailsService.java
+++ src/main/java/de/astahsrm/gremiomat/security/MgmtUserDetailsService.java
@@ -3,18 +3,15 @@ package de.astahsrm.gremiomat.security;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.User;
 
 @Service
 public class MgmtUserDetailsService implements UserDetailsService {
     @Autowired
     private MgmtUserRepository mgmtUserRepository;
-    @Autowired
-    private PasswordEncoder encoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
@@ -26,6 +23,6 @@ public class MgmtUserDetailsService implements UserDetailsService {
         }
         user = userOptional.get();
 
-        return User.withUsername(username).password(encoder.encode(user.getPassword())).roles(user.getRole()).build();
+        return User.withUsername(username).password(user.getPassword()).roles(user.getRole()).build();
     }
 }
