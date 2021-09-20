55df9c784f39f4f8cb897f8acd84c48749ed324f
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-09-20 20:24:02 +0200

    add: simple email properties

--- src/main/java/de/astahsrm/gremiomat/mail/MailServiceImpl.java
+++ src/main/java/de/astahsrm/gremiomat/mail/MailServiceImpl.java
@@ -4,8 +4,11 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import de.astahsrm.gremiomat.candidate.Candidate;
 
 @Service
 public class MailServiceImpl implements MailService {
@@ -19,18 +22,17 @@ public class MailServiceImpl implements MailService {
     @Override
     public void sendWelcomeMailToCandidate(Candidate candidate, String password) {
         /*
-        @Autowired
-        public SimpleMailMessage template;
+         * @Autowired public SimpleMailMessage template;
+         * 
+         * String text = String.format(template.getText(), templateArgs);
+         * sendSimpleMessage(to, subject, text);
+         */
 
-        String text = String.format(template.getText(), templateArgs);  
-        sendSimpleMessage(to, subject, text);
-        */
-        
-        SimpleMailMessage message = new SimpleMailMessage(); 
-        message.setFrom("[email protected]");
-        message.setTo(to); 
-        message.setSubject(subject); 
-        message.setText(text);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(candidate.getEmail());
+        message.setSubject("Example subject");
+        message.setText("Example text");
         emailSender.send(message);
     }
 }
