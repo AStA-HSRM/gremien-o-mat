ba028c4e7785a2db60ce3a694a69a9ebd09e62e4
Nico Ismaili <nico@ismaili.de>
2021-09-23 14:17:59 +0200

    fix: updated getter for gremien to check list of gremien

--- src/main/java/de/astahsrm/gremiomat/query/EditQueryForm.java
+++ src/main/java/de/astahsrm/gremiomat/query/EditQueryForm.java
@@ -1,5 +1,6 @@
 package de.astahsrm.gremiomat.query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.astahsrm.gremiomat.gremium.Gremium;
@@ -9,29 +10,53 @@ public class EditQueryForm {
     private String reason;
     private int opinion;
     private List<Gremium> gremien;
+    private long id;
 
     public String getQueryTxt() {
         return queryTxt;
     }
+
     public void setQueryTxt(String queryTxt) {
         this.queryTxt = queryTxt;
     }
+
     public String getReason() {
         return reason;
     }
+
     public void setReason(String reason) {
         this.reason = reason;
     }
+
     public int getOpinion() {
         return opinion;
     }
+
     public void setOpinion(int opinion) {
         this.opinion = opinion;
     }
+
     public List<Gremium> getGremien() {
         return gremien;
     }
+
     public void setGremien(List<Gremium> gremien) {
-        this.gremien = gremien;
+        if(this.gremien == null) {
+            this.gremien = new ArrayList<>();
+        }
+        for (int i = 0; i < gremien.size(); i++) {
+            if (gremien.get(i) instanceof Gremium) {
+                this.gremien.add(gremien.get(i));
+            }
+        }
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
 }
