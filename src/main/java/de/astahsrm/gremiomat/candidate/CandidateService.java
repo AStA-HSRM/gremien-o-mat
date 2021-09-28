9300354773aac525ff21ec3e931715829a5ff1eb
Nico Ismaili <nico@ismaili.de>
2021-09-28 13:13:37 +0200

    add: new Constant for Candidate not Found Errors

--- src/main/java/de/astahsrm/gremiomat/candidate/CandidateService.java
+++ src/main/java/de/astahsrm/gremiomat/candidate/CandidateService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CandidateService {
+    String CANDIDATE_NOT_FOUND = "No such Candidate exists.";
     public Candidate saveCandidate(Candidate candidate);
     public Optional<Candidate> getCandidateById(String candidateEmail);
     public void delCandidate(String candidateEmail);
