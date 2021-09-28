fb967a9a77c2e71cd58cfdcaf010f410076c4fb9
Nico Ismaili <nico@ismaili.de>
2021-09-28 20:42:25 +0200

    fix: simplified and updated data.sql to fit new Candidate.java

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,18 +7,11 @@ INSERT INTO query (id, version, text) VALUES
   (-2,0,'Dieser Aussage soll nicht zugestimmt werden.'),
   (-3,0,'Gegenüber dieser Frage sollte man neutral eingestimmt sein.');
 
-INSERT INTO candidate (version, firstname, lastname, email) VALUES
-(0,'Darth','Vader','1@mail.de'),
-(0,'Kim','Kardashian','2@mail.de'),
-(0,'Robert','McClanahan','3@mail.de'),
-(0,'Virginia','Davis','4@mail.de'),
-(0,'Christine','Powell','5@mail.de'),
-(0,'Peggy','Kaufman','6@mail.de'),
-(0,'Charlotte L','Galvan','7@mail.de'),
-(0,'Charles K','Galvan','8@mail.de'),
-(0,'Hector','Toro','9@mail.de'),
-(0,'Tracy L','Stack','10@mail.de'),
-(0,'Linda J','Alvarado','11@mail.de');
+INSERT INTO candidate (id, version, firstname, lastname, email) VALUES
+(-20,0,'Darth','Vader','1@mail.de'),
+(-21,0,'Kim','Kardashian','2@mail.de'),
+(-22,0,'Robert','McClanahan','3@mail.de'),
+(-23,0,'Virginia','Davis','4@mail.de');
 
 INSERT INTO candidate_answer (id, version, query_id, opinion, reason) VALUES
   (-31,0,-1,1,'Weil ich der erste Kandidat bin!'),
@@ -34,29 +27,25 @@ INSERT INTO candidate_answer (id, version, query_id, opinion, reason) VALUES
   (-311,0,-2,-1,'Weil ich der vierte Kandidat bin!'),
   (-312,0,-3,0,'Weil ich der vierte Kandidat bin!');
 
-INSERT INTO mgmt_user (version, username, password, role, candidate_details_email) VALUES
-(0,'user','{noop}user','USER','1@mail.de'),
+INSERT INTO mgmt_user (version, username, password, role, candidate_details_id) VALUES
+(0,'user','{noop}user','USER',-20),
 (0,'admin','{noop}admin', 'ADMIN', NULL);
 
-INSERT INTO candidate_answers (answers_id, candidate_email) VALUES
-(-31,'1@mail.de'),
-(-32,'1@mail.de'),
-(-33,'1@mail.de'),
-(-34,'2@mail.de'),
-(-35,'2@mail.de'),
-(-36,'2@mail.de'),
-(-37,'3@mail.de'),
-(-38,'3@mail.de'),
-(-39,'3@mail.de'),
-(-310,'4@mail.de'),
-(-311,'4@mail.de'),
-(-312,'4@mail.de');
+INSERT INTO candidate_answers (answers_id, candidate_id) VALUES
+(-31,-21),
+(-32,-21),
+(-33,-21),
+(-34,-22),
+(-35,-22),
+(-36,-22),
+(-37,-23),
+(-38,-23),
+(-39,-23);
 
 INSERT INTO candidate_join (gremium_id, candidate_id) VALUES
-('dtg','1@mail.de'),
-('dtg','2@mail.de'),
-('dtg','3@mail.de'),
-('dtg','4@mail.de');
+('dtg',-20),
+('dtg',-21),
+('dtg', -22);
 
 INSERT INTO query_contain (gremium_id, query_id) VALUES
 ('dtg',-1),
