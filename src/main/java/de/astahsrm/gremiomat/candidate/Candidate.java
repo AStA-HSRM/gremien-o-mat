19ed3d5ead98ba8791478a576b6edd0240927ab0
Nico Ismaili <nico@ismaili.de>
2021-10-02 15:07:07 +0200

    fix: made bio to large object

--- src/main/java/de/astahsrm/gremiomat/candidate/Candidate.java
+++ src/main/java/de/astahsrm/gremiomat/candidate/Candidate.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -56,6 +57,7 @@ public class Candidate {
     @Column(columnDefinition = "integer default 0")
     private int semester;
 
+    @Lob
     private String bio;
 
     private String course;
