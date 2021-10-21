0973db088c8d3b6d8cae7f4f860702764e2bf0ae
Nico Ismaili <nico@ismaili.de>
2021-10-21 20:28:41 +0200

    del: unnecessary test

--- src/test/java/de/astahsrm/gremiomat/GremiomatApplicationTests.java
+++ src/test/java/de/astahsrm/gremiomat/GremiomatApplicationTests.java
@@ -1,25 +1,8 @@
 package de.astahsrm.gremiomat;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import de.astahsrm.gremiomat.password.PasswordServiceImpl;
 
 @SpringBootTest
 class GremiomatApplicationTests {
-
-	@Test
-	public void whenPasswordGeneratedUsingCommonsLang3_thenSuccessful() {
-		String password = new PasswordServiceImpl().generatePassword();
-		int numCount = 0;
-		for (char c : password.toCharArray()) {
-			if (c >= 48 || c <= 57) {
-				numCount++;
-			}
-		}
-		assertTrue(numCount >= 2, "Password validation failed in commons-lang3");
-	}
 
 }
