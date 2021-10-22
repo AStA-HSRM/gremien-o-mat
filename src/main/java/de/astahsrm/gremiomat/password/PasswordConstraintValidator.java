0658f10dd3231c32a0966165ddcdea0254691c50
Nico Ismaili <nico@ismaili.de>
2021-10-22 11:58:25 +0200

    add: Password Validator Annotation

---
+++ src/main/java/de/astahsrm/gremiomat/password/PasswordConstraintValidator.java
@@ -0,0 +1,40 @@
+package de.astahsrm.gremiomat.password;
+
+import java.util.Arrays;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.google.common.base.Joiner;
+
+import org.passay.DigitCharacterRule;
+import org.passay.LengthRule;
+import org.passay.PasswordData;
+import org.passay.PasswordValidator;
+import org.passay.RuleResult;
+import org.passay.SpecialCharacterRule;
+import org.passay.UppercaseCharacterRule;
+import org.passay.WhitespaceRule;
+
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+            //Between 8 and 30 chars, 1 uppercase, 1 number, 1 special char
+            new LengthRule(8, 30),
+            new UppercaseCharacterRule(1), 
+            new DigitCharacterRule(1), 
+            new SpecialCharacterRule(1),
+            new WhitespaceRule()));
+
+        RuleResult result = validator.validate(new PasswordData(password));
+        if (result.isValid()) {
+            return true;
+        }
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(Joiner.on(",").join(validator.getMessages(result)))
+                .addConstraintViolation();
+        return false;
+    }
+}
---
+++ src/main/java/de/astahsrm/gremiomat/password/ValidPassword.java
@@ -0,0 +1,25 @@
+package de.astahsrm.gremiomat.password;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+
+import org.springframework.messaging.handler.annotation.Payload;
+
+@Documented
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Target({ ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+
+    String message() default "Invalid Password";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
