f923ef112dfba142c741c950338bb735f208c4f9
Nico Ismaili <nico@ismaili.de>
2021-10-22 13:30:08 +0200

    add: validation to CandidateDto

--- src/main/java/de/astahsrm/gremiomat/candidate/CandidateDto.java
+++ src/main/java/de/astahsrm/gremiomat/candidate/CandidateDto.java
@@ -1,12 +1,34 @@
 package de.astahsrm.gremiomat.candidate;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import de.astahsrm.gremiomat.constraints.semesterandcourse.SemesterAndCourse;
+
 public class CandidateDto {
 
+    @Min(0)
+    @Max(120)
     private int age;
+
+    @Min(1)
+    @Max(20)
+    @SemesterAndCourse
     private int semester;
+
+    @NotBlank
     private String firstname;
+
+    @NotBlank
     private String lastname;
+
+    @Size(max = 50)
+    @SemesterAndCourse
     private String course;
+
+    @Size(max = 1000)
     private String bio;
 
     public int getAge() {
