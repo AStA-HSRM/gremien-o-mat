3ab47772dec126431322db2e8168567a129882ba
Nico Ismaili <nico@ismaili.de>
2021-10-24 16:36:44 +0200

    fix: changed validation for SemesterAndCourse

--- src/main/java/de/astahsrm/gremiomat/constraints/semesterandcourse/SemesterAndCourse.java
+++ src/main/java/de/astahsrm/gremiomat/constraints/semesterandcourse/SemesterAndCourse.java
@@ -3,7 +3,7 @@ package de.astahsrm.gremiomat.constraints.semesterandcourse;
 import javax.validation.Payload;
 
 public @interface SemesterAndCourse {
-    String message() default "Both Semester and course must either be both or neither entered.";
+    String message() default "Both Semester and course must be filled or neither of them.";
 
     Class<?>[] groups() default {};
 
--- src/main/java/de/astahsrm/gremiomat/constraints/semesterandcourse/SemesterAndCourseConstraintValidator.java
+++ src/main/java/de/astahsrm/gremiomat/constraints/semesterandcourse/SemesterAndCourseConstraintValidator.java
@@ -9,7 +9,7 @@ public class SemesterAndCourseConstraintValidator implements ConstraintValidator
  
     @Override
     public boolean isValid(CandidateDto form, ConstraintValidatorContext context) {
-        return (!form.getCourse().isBlank() && form.getSemester() != 0) || (form.getCourse().isBlank() && form.getSemester() == 0);
+        return !form.getCourse().isBlank() && form.getSemester() != 0;
     }
  
 }
