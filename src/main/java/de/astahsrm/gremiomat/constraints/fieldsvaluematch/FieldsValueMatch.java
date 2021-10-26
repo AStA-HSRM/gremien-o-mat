227775916810d82ef46869a4e9ab32e729381176
Nico Ismaili <nico@ismaili.de>
2021-10-26 21:08:51 +0200

    add: verifyPassword field and corresponding validator

---
+++ src/main/java/de/astahsrm/gremiomat/constraints/fieldsvaluematch/FieldsValueMatch.java
@@ -0,0 +1,32 @@
+package de.astahsrm.gremiomat.constraints.fieldsvaluematch;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+
+import org.springframework.messaging.handler.annotation.Payload;
+
+@Constraint(validatedBy = FieldsValueMatchValidator.class)
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldsValueMatch {
+
+    String message() default "Fields values don't match!";
+
+    String field();
+
+    String fieldMatch();
+
+    @Target({ ElementType.TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        FieldsValueMatch[] value();
+    }
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
--- src/main/java/de/astahsrm/gremiomat/password/PasswordDto.java
+++ src/main/java/de/astahsrm/gremiomat/password/PasswordDto.java
@@ -1,11 +1,18 @@
 package de.astahsrm.gremiomat.password;
 
+import javax.validation.constraints.NotBlank;
+
+import de.astahsrm.gremiomat.constraints.fieldsvaluematch.FieldsValueMatch;
 import de.astahsrm.gremiomat.constraints.validpassword.ValidPassword;
 
+@FieldsValueMatch(field = "newPassword", fieldMatch = "verifyPassword")
 public class PasswordDto {
     
     @ValidPassword
     private String newPassword;
+
+    @NotBlank
+    private String verifyPassword;
 
     public String getNewPassword() {
         return newPassword;
@@ -13,6 +20,14 @@ public class PasswordDto {
 
     public void setNewPassword(String newPassword) {
         this.newPassword = newPassword;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
     }
 
 }
