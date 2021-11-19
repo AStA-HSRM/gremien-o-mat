eda64b62d0d9caa8fe3928ba26beb2c8f660de2d
Nico Ismaili <nico@ismaili.de>
2021-11-19 15:31:35 +0100

    add: automatic image cropping and squaring

--- src/main/java/de/astahsrm/gremiomat/photo/PhotoService.java
+++ src/main/java/de/astahsrm/gremiomat/photo/PhotoService.java
@@ -1,13 +1,16 @@
 package de.astahsrm.gremiomat.photo;
 
+import java.io.IOException;
 import java.util.Optional;
 
 public interface PhotoService {
 
-    public Photo save(Photo photo);
+    public Photo save(Photo photo) throws IOException;
 
     public Optional<Photo> getPhotoById(long id);
 
     public void delPhoto(Photo photo);
+
+    public byte[] cropImageSquare(byte[] image, String format) throws IOException;
 
 }
--- src/main/java/de/astahsrm/gremiomat/photo/PhotoServiceImpl.java
+++ src/main/java/de/astahsrm/gremiomat/photo/PhotoServiceImpl.java
@@ -1,6 +1,13 @@
 package de.astahsrm.gremiomat.photo;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +19,8 @@ public class PhotoServiceImpl implements PhotoService {
     private PhotoRepository photoRepository;
 
     @Override
-    public Photo save(Photo photo) {
+    public Photo save(Photo photo) throws IOException {
+        photo.setBytes(cropImageSquare(photo.getBytes(), photo.getMimeType()));
         return photoRepository.save(photo);
     }
 
@@ -24,6 +32,35 @@ public class PhotoServiceImpl implements PhotoService {
     @Override
     public void delPhoto(Photo photo) {
         photoRepository.delete(photo);
+    }
+
+    @Override
+    public byte[] cropImageSquare(byte[] image, String format) throws IOException {
+        // Get a BufferedImage object from a byte array
+        InputStream in = new ByteArrayInputStream(image);
+        BufferedImage originalImage = ImageIO.read(in);
+
+        // Get image dimensions
+        int height = originalImage.getHeight();
+        int width = originalImage.getWidth();
+
+        // The image is already a square
+        if (height == width) {
+            return image;
+        }
+
+        // Compute the size of the square
+        int squareSize = (height > width ? width : height);
+
+        // Coordinates of the image's middle
+        int xc = width / 2;
+        int yc = height / 2;
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        BufferedImage croppedImage = originalImage.getSubimage(xc - (squareSize / 2), yc - (squareSize / 2), squareSize, squareSize);
+        ImageIO.write(croppedImage, format.replace("image/", ""), baos);
+        
+        return baos.toByteArray();
     }
 
 }
